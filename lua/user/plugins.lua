1378200b844f87b9bcf3245cbf0606ad09b8f291
Hadi4234 <abdullahalhadi71852@gmail.com>
2023-01-27 11:06:43 +0600

    add harpon and undo pluging

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -92,6 +92,9 @@ return packer.startup(function(use)
 	-- Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
 
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
